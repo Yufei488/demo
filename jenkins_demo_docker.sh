3c843542dde35c7c595f469da4660dd0cba43958
yufei <435977043@qq.com>
2021-01-10 00:24:46 +0800

    jenkins-demo

--- jenkins_demo_docker.sh
+++ jenkins_demo_docker.sh
@@ -2,12 +2,12 @@ echo '-----开始生成镜像-----'
 app_name=jenkins_demo
 app_version=0.0.1
 docker_file_path=/program/data/jenkins01/workspace/jenkins_demo/target/classes/Dockerfile
-echo '----------'$docker_file_path
-docker stop ${app_name}
-echo '----------'
-docker rmi ${app_name}
-echo '----------'
+echo '-----终止正在运行-----'
+docker stop $(docker images | grep jenkins_demo)
+echo '-----删除旧镜像-----'
+docker rmi -f $(docker images | grep jenkins_demo)
+echo '-----创建镜像-----'
 docker build -f ${docker_file_path} -t ${app_name}:${app_version} .
-echo '----------'
+echo '-----运行镜像-----'
 docker run --name ${app_name} ${app_name}:${app_version}
 echo '----------'
